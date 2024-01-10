fb2b45d70e341710acd454d66c37da50c8122ed6
ELMADDAH Hajar <hajar.elmaddah@telecomnancy.eu>
2024-01-10 11:41:21 +0100

    profil

--- src/main/java/eu/telecomnancy/labfx/Main.java
+++ src/main/java/eu/telecomnancy/labfx/Main.java
@@ -38,7 +38,7 @@ public class Main extends Application {
     public void loginScreen() throws IOException {
         FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/Login.fxml"));
 
-        // Assuming you have a LoginController class for the login screen
+       
         fxmloader.setControllerFactory(ic -> new Login(this));
 
         Scene scene = new Scene(fxmloader.load(), 400, 400);
@@ -49,7 +49,7 @@ public class Main extends Application {
     public void createAccountScreen() throws IOException {
         FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/create_compte.fxml"));
     
-        // Assuming you have a CreateAccountController class for the account creation screen
+        
         fxmloader.setControllerFactory(ic -> new CreateAccount(this));
     
         Scene scene = new Scene(fxmloader.load(), 400, 400);
@@ -85,6 +85,19 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public void ViewCompteController(User user) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/ViewCompte.fxml"));
+        Parent root = loader.load();
+
+        ViewCompteController controller = loader.getController();
+        controller.setUser(user);
+        controller.setMain(this);
+
+        Scene scene = new Scene(root, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
     public void viewOffer(User user, Ad offer) throws IOException{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/ViewOffer.fxml"));
         Parent root = loader.load();
--- src/main/java/eu/telecomnancy/labfx/model/Ad.java
+++ src/main/java/eu/telecomnancy/labfx/model/Ad.java
@@ -1,5 +1,6 @@
 package eu.telecomnancy.labfx.model;
 
+import java.net.URI;
 import java.util.Date;
 
 public class Ad {
@@ -17,4 +18,123 @@ public class Ad {
   public Date end;
   public int duration;
   public String disponibilities;
+  public String getPhotoPath() {
+    return imagePath;
+  }
+  public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isOffer() {
+        return isOffer;
+    }
+
+    public void setOffer(boolean offer) {
+        isOffer = offer;
+    }
+
+    public AdType getType() {
+        return type;
+    }
+
+    public void setType(AdType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getMaxDistance() {
+        return maxDistance;
+    }
+
+    public void setMaxDistance(double maxDistance) {
+        this.maxDistance = maxDistance;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDisponibilities() {
+        return disponibilities;
+    }
+
+    public void setDisponibilities(String disponibilities) {
+        this.disponibilities = disponibilities;
+    }
+
 }
+
+
+    
+
